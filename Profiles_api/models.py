81fe792241b55326366d0803a6ef46e84c2be267
Himanshu <31452165+h96Coder@users.noreply.github.com>
2020-02-13 14:06:57 +0530

    Added Project and app

---
+++ Profiles_api/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser,PermissionsMixin,BaseUserManager
+
+
+class UserProfileManger(BaseUserManager):
+
+    def create_user(self,email,name,password):
+        if not email:
+           raise  ValueError("Email not entered")
+        email =self.normalize_email(email)
+        user = self.model(email=email,name=name)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    def create_superuser(self,email,name,password):
+        user= self.create_user(email,name,password)
+        user.is_superuser=True
+        user.is_staff=True
+        user.save(using=self._db)
+
+
+
+class UserProfile(AbstractUser,PermissionsMixin):
+    email= models.EmailField(max_length=200,unique=True)
+    name = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManger()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+    def get_full_name(self):
+        return self.name
+    def get_short_name(self):
+        return self.name
+    def __str__(self):
+        return self.email
+
+# Create your models here.
