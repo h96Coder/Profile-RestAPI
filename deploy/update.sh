a3df1edb3c4b1ded856592d2446ebbb7e643856b
Himanshu <31452165+h96Coder@users.noreply.github.com>
2020-02-24 01:15:53 +0530

    Host added

--- deploy/update.sh
+++ deploy/update.sh
@@ -7,6 +7,6 @@ PROJECT_BASE_PATH='/usr/local/apps/Profiles-RestAPI'
 git pull
 $PROJECT_BASE_PATH/ py manage.py migrate
 $PROJECT_BASE_PATH/ py manage.py collectstatic --noinput
-supervisorctl restart profiles_api
+supervisorctl restart Profiles_api
 
 echo "DONE! :)"
